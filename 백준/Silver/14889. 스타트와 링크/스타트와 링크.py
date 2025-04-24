54e2e4d7361a871dca971ab214f53cbc7c966d40
Youngjun Yoo <yoo156@purdue.edu>
2025-04-24 14:10:44 -0400

    [Silver I] Title: 스타트와 링크, Time: 3744 ms, Memory: 57548 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,25 @@
+import itertools
+import sys
+
+def main():
+    N = int(sys.stdin.readline())
+    board = [list(map(int, sys.stdin.readline().split(" "))) for _ in range(N)]
+    # genereate only the start team to avoid MLE
+    start = list(itertools.combinations(list(range(1, N + 1)), N // 2))    
+    min_diff = float('inf')
+    for s in start:
+        # generate the link team based on the each start team
+        link = [p for p in list(range(1, N + 1)) if p not in s]
+        start_ability, link_ability = 0, 0
+        for p in itertools.permutations(s, 2):
+            # subtract 1 bc board is 1-based index
+            start_ability += board[p[0] - 1][p[1] - 1]
+        for p in itertools.permutations(link, 2):
+            link_ability += board[p[0] - 1][p[1] - 1]
+        min_diff = min(min_diff, abs(start_ability - link_ability))
+        if min_diff == 0:
+            break
+    print(min_diff)
+    
+if __name__ == "__main__":
+    main()
