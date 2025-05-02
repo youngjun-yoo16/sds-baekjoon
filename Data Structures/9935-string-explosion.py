94ae41eaf6c618f41a4df9041beb92735ee71d68
Youngjun Yoo <yoo156@purdue.edu>
2025-05-02 13:05:31 -0400

    solve: string explosion

--- Data Structures/9935-string-explosion.py
+++ Data Structures/9935-string-explosion.py
@@ -3,8 +3,17 @@ import sys
 def main():
     string = list(sys.stdin.readline().strip())
     explosion_string = list(sys.stdin.readline().strip())
+    stack = []
 
+    for s in string:
+        stack.append(s)
+        # if the last character matches and stack ends with the explosion string
+        if s == explosion_string[-1] and stack[-len(explosion_string):] == explosion_string:
+            # pop the explosion string from the stack
+            for _ in range(len(explosion_string)):
+                stack.pop()
+    
+    print(''.join(stack)) if stack else print("FRULA")
 
-    print(string)
 if __name__=="__main__":
     main()
