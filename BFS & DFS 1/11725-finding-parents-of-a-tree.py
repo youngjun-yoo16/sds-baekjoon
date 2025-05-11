5d65129624b1eee249baed4b7a8c5d04f2bc4e74
Youngjun Yoo <yoo156@purdue.edu>
2025-05-11 03:06:32 -0400

    solve: finding parents of a tree

--- BFS & DFS 1/11725-finding-parents-of-a-tree.py
+++ BFS & DFS 1/11725-finding-parents-of-a-tree.py
@@ -3,14 +3,30 @@ import collections
 
 def main():
     N = int(sys.stdin.readline().strip())
+    # Adjacency list to store the tree
     nodeMap = collections.defaultdict(list)
-    
+    # Array to store the parent of each node
+    parents = [0] * (N + 1)
+    # Initialize BFS queue with root node
+    q = collections.deque([1])
+
     for _ in range(N - 1):
         x, y = map(int, sys.stdin.readline().split(" "))
-        
-            
+        nodeMap[x].append(y)
+        nodeMap[y].append(x)
     
-    print(nodeMap)
+    # BFS
+    while q:
+        node = q.popleft()
+        for children in nodeMap[node]:
+            # To prevent revisiting the parent node
+            if children != parents[node]:
+                # Assign parent
+                parents[children] = node
+                q.append(children)
+    
+    for i in range(2, N + 1):
+        print(parents[i])
 
 if __name__=="__main__":
     main()
