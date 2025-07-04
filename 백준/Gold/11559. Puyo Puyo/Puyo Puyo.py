2de45e2aa80a3ce833b19117f127697410a7a6b7
Youngjun Yoo <yoo156@purdue.edu>
2025-07-04 14:21:24 +0900

    [Gold IV] Title: Puyo Puyo, Time: 60 ms, Memory: 35084 KB -BaekjoonHub

---
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -0,0 +1,88 @@
+from collections import deque
+
+
+def main():
+    grid = [list(input()) for _ in range(12)]
+    N, M = len(grid), len(grid[0])
+
+    def burst(l, r, c):
+        q = deque([(r, c)])
+        visited = {(r, c)}
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < N and 0 <= nc < M and grid[nr][nc] == l:
+                    grid[nr][nc] = '.'
+                    q.append((nr, nc))
+                    visited.add((nr, nc))
+
+    def find_and_burst():
+        nonlocal found
+        visited = set([])
+
+        def bfs(letter, r, c):
+            nonlocal found, visited
+
+            q = deque([(r, c)])
+            visited.add((r, c))
+            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+            num_letter = 1
+
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < N and 0 <= nc < M and grid[nr][nc] == letter and (nr, nc) not in visited:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+                        num_letter += 1
+
+            if num_letter >= 4:
+                return letter, r, c
+
+            return -1, -1, -1
+
+        for r in range(N):
+            for c in range(M):
+                if grid[r][c] != '.' and (r, c) not in visited:
+                    l, i, j = bfs(grid[r][c], r, c)
+                    if l != -1:
+                        found = True
+                        burst(l, i, j)
+
+    def fall_down():
+        for i in range(N - 1):
+            for j in range(M):
+                p = i
+                while p >= 0 and grid[p][j] != '.' and grid[p + 1][j] == '.':
+                    grid[p][j], grid[p + 1][j] = grid[p + 1][j], grid[p][j]
+                    p -= 1
+
+    found, count = False, 0
+    while True:
+        find_and_burst()
+
+        # for row in grid:
+        #     print(*row, sep='')
+        #
+        # print("---------------")
+
+        if not found:
+            break
+
+        fall_down()
+
+        # for row in grid:
+        #     print(*row, sep='')
+
+        count += 1
+
+        found = False
+
+    print(count)
+
+if __name__ == '__main__':
+    main()
